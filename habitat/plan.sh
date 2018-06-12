5ddd37a08c2d0ca0476adb3e2b3ac2bd27664e9d
Cesar Felce <cfelce@gmail.com>
2018-06-12 12:35:13 -0400

    Hello Felce bldr from git

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=keepex-dashboard
 pkg_origin=learn-binpath
-pkg_version="0.1.0"
+pkg_version="0.1.1"
 pkg_maintainer="Novis <cesar.felce@noviscorp.com>"
 pkg_license=("Apache-2.0")
 pkg_deps=(core/node)
